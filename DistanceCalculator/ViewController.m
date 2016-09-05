8f753a5af4899fbf29fb1f5fdd4ba3ee978ef038
Alan Glasby <alan@glasby.me.uk>
2016-09-05 15:14:23 +0100

    outlets and actions connected

--- DistanceCalculator/ViewController.m
+++ DistanceCalculator/ViewController.m
@@ -9,6 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *startPointTxtFld;
+@property (weak, nonatomic) IBOutlet UITextField *destination1TxtFld;
+@property (weak, nonatomic) IBOutlet UITextField *destination2TxtFld;
+@property (weak, nonatomic) IBOutlet UITextField *destination3TxtFld;
+@property (weak, nonatomic) IBOutlet UILabel *distance1Lbl;
+@property (weak, nonatomic) IBOutlet UILabel *distance2LBL;
+@property (weak, nonatomic) IBOutlet UILabel *distance3Lbl;
 
 @end
 
@@ -18,5 +25,31 @@
     [super viewDidLoad];
 }
 
+- (IBAction)getDistancesPushed:(id)sender {
+
+}
+
+-(void)configureTxtFlds {
+    NSMutableArray *txtFlds = [[NSMutableArray alloc] initWithCapacity:4];
+    [txtFlds addObject:_startPointTxtFld];
+    [txtFlds addObject:_destination1TxtFld];
+    [txtFlds addObject:_destination2TxtFld];
+    [txtFlds addObject:_destination3TxtFld];
+
+    UITextField *tempTextFld = [txtFlds objectAtIndex:0];
+    tempTextFld.layer.cornerRadius = 8.0;
+//    tempTextFld.layer.borderColor = 
+
+
+
+}
+
+-(void)configureLbls {
+
+}
+
+-(void)configureBtn {
+
+}
 
 @end
