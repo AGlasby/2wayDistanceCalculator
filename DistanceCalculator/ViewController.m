269fccc3a47bfe350195e46535f73f23f9e35d80
Alan Glasby <alan@glasby.me.uk>
2016-09-07 09:56:07 +0100

    Completed assignment including change of units.

--- DistanceCalculator/ViewController.m
+++ DistanceCalculator/ViewController.m
@@ -19,10 +19,14 @@
 @property (weak, nonatomic) IBOutlet UITextField *destination3TxtFld;
 @property (weak, nonatomic) IBOutlet UITextField *destination4TxtFld;
 @property (weak, nonatomic) IBOutlet UILabel *distance1Lbl;
-@property (weak, nonatomic) IBOutlet UILabel *distance2LBL;
+@property (weak, nonatomic) IBOutlet UILabel *distance2Lbl;
 @property (weak, nonatomic) IBOutlet UILabel *distance3Lbl;
 @property (weak, nonatomic) IBOutlet UILabel *distance4Lbl;
 @property (weak, nonatomic) IBOutlet UIButton *getDistanceBtn;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *distanceUnitsSegmentedController;
+
+@property (nonatomic) double divisor;
+@property (nonatomic) NSString *distanceUnits;
 
 @end
 
@@ -34,12 +38,33 @@
     [self configureTxtFlds];
     [self configureLbls];
     [self configureBtn];
+    self.divisor = 1.00;
+    self.distanceUnits = @"m";
 }
+
 - (IBAction)distanceUnitsSegmentedControl:(id)sender {
-    
+
+    switch (self.distanceUnitsSegmentedController.selectedSegmentIndex) {
+        case 0:
+            self.divisor = 1.00;
+            self.distanceUnits = @"m";
+            break;
+        case 1:
+            self.divisor = 1000.0;
+            self.distanceUnits = @"km";
+            break;
+        case 2:
+            self.divisor = 1609.344;
+            self.distanceUnits = @"mi";
+            break;
+        default:
+            break;
+    }
 }
 
 - (IBAction)getDistancesPushed:(id)sender {
+    
+    if(![self.startPointTxtFld.text  isEqualToString:@"Enter starting location"] && ![self.startPointTxtFld.text isEqualToString:@""]) {
 
     self.getDistanceBtn.enabled = NO;
 
@@ -52,6 +77,13 @@
 
     NSArray *destinations = @[end1, end2, end3, end4];
 
+    UILabel *distance1 = self.distance1Lbl;
+    UILabel *distance2 = self.distance2Lbl;
+    UILabel *distance3 = self.distance3Lbl;
+    UILabel *distance4 = self.distance4Lbl;
+
+    NSArray *distances = @[distance1, distance2, distance3, distance4];
+
     self.req = [self.req initWithLocationDescriptions:destinations sourceDescription:start];
 
     __weak ViewController *weakSelf = self;
@@ -61,19 +93,37 @@
         if(!strongSelf) return;
 
         NSNull *badResult = [NSNull null];
-        double dist = ([responses[0] floatValue] / 1000.0);
-        if(responses[0] != badResult) {
-            NSString *distance = [NSString stringWithFormat:@"  %.2f km", (dist)];
-            strongSelf.distance1Lbl.text = distance;
-        } else {
-            strongSelf.distance1Lbl.text = @"  Error";
+
+        for (int i = 0; i < [responses count]; i++) {
+            double dist = ([responses[i] floatValue] / self.divisor);
+            NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+            [numberFormatter setLocale:[NSLocale currentLocale]];
+            [numberFormatter setNumberStyle:NSNumberFormatterDecimalStyle];
+            [numberFormatter setMaximumFractionDigits:2];
+            NSString *formattedDist = [numberFormatter stringFromNumber:[NSNumber numberWithDouble:(dist)]];
+
+            if(responses[i] != badResult) {
+                NSString *distance = [NSString stringWithFormat:@"  %@ %@", (formattedDist), (strongSelf.distanceUnits)];
+                UILabel *labelToUpdate = [distances objectAtIndex:i];
+                labelToUpdate.text = distance;
+            } else {
+                UILabel *labelToUpdate = [distances objectAtIndex:i];
+                labelToUpdate.text = @"  Error";
+            }
         }
-
-    strongSelf.getDistanceBtn.enabled = YES;
-    strongSelf.req = nil;
+        strongSelf.getDistanceBtn.enabled = YES;
+        strongSelf.req = nil;
     };
-
     [self.req start];
+    } else {
+        NSLog(@"Error - start location has not been set");
+        UIAlertController *actionController = [UIAlertController alertControllerWithTitle:@"Set Start Location" message:@"Please set a valid start location for the distance calculation." preferredStyle:UIAlertControllerStyleAlert];
+
+        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleDefault handler:nil];
+
+        [actionController addAction:cancelAction];
+        [self presentViewController:actionController animated:YES completion:nil];
+    }
 }
 
 -(void)configureTxtFlds {
@@ -102,7 +152,7 @@
     NSMutableArray *lbls = [[NSMutableArray alloc] initWithCapacity:3];
 
     [lbls addObject:self.distance1Lbl];
-    [lbls addObject:self.distance2LBL];
+    [lbls addObject:self.distance2Lbl];
     [lbls addObject:self.distance3Lbl];
     [lbls addObject:self.distance4Lbl];
 
